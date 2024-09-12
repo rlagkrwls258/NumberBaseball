105d8b9b816d1a74317cd26206c2dae857a20c7e
caose <rlagkrwls258@gmail.com>
2024-09-12 11:37:56 +0900

    feat Lv1

---
+++ src/App.java
@@ -0,0 +1,109 @@
+import java.util.*;
+
+
+public class App {
+    private List<Integer> randomArr = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+    private int opt_size = 3; //기본 사이즈
+
+    public void start() {
+        // 정답을 만드는 함수
+        Scanner scanner = new Scanner(System.in);
+        String answer = makeAnswer();
+        System.out.println(answer);
+        while (true) {
+            System.out.println("숫자를 입력하세요");
+
+            // 1. 유저에게 입력값을 받음
+            String input = scanner.nextLine();
+
+            // 2. 정수로 변환되지 않는 경우 반복문 처음으로 돌아가기
+            if(!isValidNumber(input)){
+                System.out.println("올바르지 않은 입력값입니다.");
+                continue;
+            }
+
+            // 3. 세 자리가 아니거나, 0을 가지거나 특정 숫자가 두 번 사용된 경우 반복문 처음으로 돌아가기
+            if (!isThreeDigitNumber(input) || hasDuplicateDigits(input) || input.contains("0")) {
+                System.out.println("올바르지 않은 입력값입니다.");
+                continue;
+            }
+
+            // 4. 정답과 유저의 입력값을 비교하여 스트라이크/볼을 출력하기
+            // 만약 정답이라면 break 호출하여 반복문 탈출
+            if(input.equals(answer)){
+                System.out.println("정답입니다.");
+                break;
+            }else{
+                System.out.println(getStrikeAndBall(answer,input));
+            }
+
+        }
+        scanner.close();
+    }
+
+    private String makeAnswer() {
+        // 정답을 만드는 로직을 구현하세요.
+        Collections.shuffle(randomArr);
+        StringBuffer answer = new StringBuffer();
+
+        for(int i=0; i<opt_size; i++) {
+            answer.append(randomArr.get(i));
+        }
+
+        return answer.toString(); // 예제용 정답
+    }
+
+    private boolean isValidNumber(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private boolean isThreeDigitNumber(String input) {
+        if (input.length() != opt_size) {
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    private boolean containsZero(String input) {
+        if(input.contains("0")){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    private boolean hasDuplicateDigits(String input) {
+        Set<Character> temp = new HashSet<>();
+        for(char word : input.toCharArray()) {
+            if(!temp.add(word)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String getStrikeAndBall(String answer, String input) {
+        int strike = 0;
+        int ball = 0;
+
+        for(int i=0; i<answer.length(); i++) {
+            if(answer.charAt(i) == input.charAt(i)) {
+                strike++;
+            }else if(answer.contains(String.valueOf(input.charAt(i)))) {
+                ball++;
+            }
+        }
+
+        if(strike > 0 || ball > 0){
+            return strike + "스트라이크 " + ball + "볼";
+        }else{
+            return "아웃";
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        App app = new App();
+
+
+        System.out.println("< 게임을 시작합니다 >");
+        app.start();
+    }
+}
